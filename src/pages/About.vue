91ac5c72a0434a498f2373278f3162986b003924
Маратик555 <amr404@bk.ru>
2022-04-26 16:01:29 +0300

    ok

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Данное приложение было создано
+      <a target="_blank" href="https://github.com/Maratik555">Maratik555</a></h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
